eb51442b435f09d21d6e69b045a9a039a9618b1f
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-31 09:54:07 +0700

    create function handle create data only user login

--- app/Http/Controllers/API/LookbookController.php
+++ app/Http/Controllers/API/LookbookController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\LookbookModel;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Ramsey\Uuid\Uuid;
 
 class LookbookController extends Controller
 {
@@ -14,6 +18,54 @@ class LookbookController extends Controller
         return response([
             'code' => 200,
             'message' => 'success get all data ',
+            'data' => $data
+        ]);
+    }
+
+    public function getDataByUser()
+    {
+        $user = Auth::user();
+        $data = LookbookModel::where('id_user', $user->id)->first();
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+    
+    public function createData(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'date' => 'required',
+            'description' => 'required'
+        ]);
+
+
+        if ($validation->fails()) {
+            return response()->json([
+                'code' => 422,
+                'message' => 'check your validation',
+                'errors' => $validation->errors()
+            ]);
+        }
+
+        try {
+            $user = Auth::user();
+            $data = new LookbookModel;
+            $data->uuid = Uuid::uuid4()->toString();
+            $data->id_user = $user->id;
+            $data->date = $request->input('date');
+            $data->description = $request->input('description');
+            $data->save();
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'failed',
+                'errors' => $th->getMessage()
+            ]);
+        }
+
+        return response()->json([
+            'code' => 200,
+            'message' => 'success',
             'data' => $data
         ]);
     }
